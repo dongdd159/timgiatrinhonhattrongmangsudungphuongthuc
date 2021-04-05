846d4c94e3bf1e789d36099470d5c62ad29f251e
dongdd159 <79952800+dongdd159@users.noreply.github.com>
2021-04-05 15:26:17 +0700

    timgiatrinhonhattrongmangsudungphuongthuc

---
+++ src/timgiatrinhonhattrongmangsudungphuongthuc.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class timgiatrinhonhattrongmangsudungphuongthuc {
+    public static int minValue(int[] array){
+        int min =0;
+        for (int i=1;i<array.length;i++){
+            if (array[min]>array[i]){
+                min=i;
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int []arr = {4, 12, 7, 8, 1, 6, 9};
+        int index = minValue(arr);
+        System.out.println("The smallest element in the array is: " + arr[index]);
+    }
+}
